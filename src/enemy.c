97ee593c360dbbbec889978af80cc957512b8901
MAZEYRAC <nicolas.mazeyrac@edu.univ-eiffel.fr>
2021-12-23 11:06:28 +0100

    Generation of a base ennemy

--- src/enemy.c
+++ src/enemy.c
@@ -1,9 +1,38 @@
 #include "enemy.h"
+#include <stdio.h>
 
-Enemy generateEnemy(unsigned int stage) {
-	/* TODO */
+Enemy generateEnemy(unsigned int stage, unsigned int manhattan) {
+    
+    Enemy monster;
+    unsigned int stat;
+
+    monster.name = "Monster";
+
+    stat = 10*stage + manhattan;
+    monster.attack = stat;
+    monster.hp = stat;
+
+    monster.crit.modificater = 300 ; /* 300% */
+    monster.crit.rate = 5;           /* 5% */
+    monster.crit.accuracy = 20 ;    /* +/-20% */
+
+    return monster;
 }
 
-int enemyIsDead(Enemy enemy) {
-	/* TODO */
+int enemyIsDead(Enemy monster) {
+    return 0;
+}
+
+
+void quickPrintEnemy(Enemy monster){
+    printf("****Enemy STATS****\n");
+    printf("name          : %s\n", monster.name);
+
+    printf("strenght      : %d\n",  monster.attack);
+
+    printf("hp            : %d\n", monster.hp);
+
+    printf("CritModifiyer : %d%%\n", monster.crit.modificater);
+    printf("CritRate      : %d%%\n", monster.crit.rate);
+    printf("Accuracy      : %d%%\n", monster.crit.accuracy);
 }
--- src/enemy.h
+++ src/enemy.h
@@ -16,17 +16,21 @@
     /*Struct*/
 
     typedef struct {
-        Stat stat;
+        char* name;/*Name of the monster*/
+        unsigned int attack;/* Used to calculate the power of a melee attack */
+        unsigned int hp;/*Max hp*/
+        Critical crit;/* Information about critical attack */
     } Enemy;
 
 
     /*Functions*/
 
-    /* Returns an enemy which attributes will be determined by the current stage. */
-    Enemy generateEnemy(unsigned int stage);
+    /* Returns an enemy which attributes will be determined by the current stage and his distance from the stairs */
+    Enemy generateEnemy(unsigned int stage, unsigned int manhattan);
 
     /* Returns 1 if the given enemy is dead and 0 otherwise. */
     int enemyIsDead(Enemy enemy);
 
-    /* ADD OTHER FUNCTIONS HERE */
+    /*Debug*/
+    void quickPrintEnemy(Enemy enemy);
 #endif
