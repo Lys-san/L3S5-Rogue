3a60d687689f3e994a6df3be10a3f39b48c3ea94
MAZEYRAC <nicolas.mazeyrac@edu.univ-eiffel.fr>
2021-12-23 11:07:14 +0100

    Generation of a treasure

--- src/treasure.c
+++ src/treasure.c
@@ -1,9 +1,131 @@
 #include "treasure.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
-Loot generateLoot(unsigned int stage, unsigned int rarityRate) {
-	/* TODO */
+
+Consummables generateConsummable(void){
+	int randomnumber;
+
+	randomnumber = rand() % 5;
+
+	switch(randomnumber){
+		case 0:return HEAL;
+		case 1:return MAGIC;
+		case 2:return REGEN;
+		case 3:return LEARNING;
+		case 4:return PRECISION;
+		default :printf("ERROR: randomnumber = %d \n", randomnumber);
+	}
+
+	return EMPTY;
 }
 
-Treasure generateTreasure(unsigned int stage, unsigned int rarity) {
-	/* TODO */
+Equipment generateEquipment(unsigned int quality, unsigned int rarity){
+	
+	Equipment equipment;
+	int randomnumber;
+
+	equipment.quality = quality;
+	equipment.rarity = rarity;
+
+	randomnumber = rand() % 3;
+
+	switch(randomnumber){
+		case 0:equipment.type = ARMOR;break;
+		case 1:equipment.type = WEAPON;break;
+		case 2:equipment.type = WAND;break;
+		default :printf("ERROR: randomnumber = %d \n", randomnumber);
+	}
+
+	return equipment;
+}
+
+Loot generateLoot(unsigned int quality, unsigned int rarity, LootType type) {
+	Loot loot;
+
+	loot.type = type;
+	if(loot.type == CONSUMMABLE){
+		loot.consummable = generateConsummable();
+	}
+	else{
+		loot.equipment = generateEquipment(quality, rarity);
+	}
+	return loot;
+}
+
+Treasure generateTreasure(unsigned int stage, unsigned int playerlevel) {
+	
+	int i;
+	int rarity, quality, type;
+	Treasure chest;
+
+	srand( time( NULL ) );
+
+	/*Fills the two items of the treasure chest*/
+	for( i = 0 ; i < 2 ; i++ ){
+
+		/* calculate rarity */
+		rarity = rand() % 4 + 1;/*random beetwen 1 and 5*/
+
+		/* calculate type Equip/Consummable*/
+		type = rand() % 2 + 1;/*random beetwen 1 and 2*/
+
+		/* calculate quality */
+		quality = rand() % stage + 1;/*random beetwen 1 and the level of stage*/
+
+		if(type == 1){
+			chest.loot[i] = generateLoot(quality, rarity, EQUIPMENT);
+		}
+		else{
+			chest.loot[i] = generateLoot(quality, rarity, CONSUMMABLE);
+		}
+	}
+	return chest;
+}
+
+
+void quickPrintEquipment(Equipment equip){
+	switch(equip.type){
+		case ARMOR: printf("ARMOR \n");break;
+		case WEAPON: printf("WEAPON \n");break;
+		case WAND: printf("WAND \n");break;
+		default: printf("ERROR \n");
+	}
+	printf("- rarity : %d \n", equip.rarity);
+	printf("- quality : %d \n", equip.quality);
+}
+
+void quickPrintConsummable(Consummables consummable){
+	switch(consummable){
+		case HEAL: printf("HEAL \n");break;
+		case MAGIC: printf("MAGIC \n");break;
+		case REGEN: printf("REGEN \n");break;
+		case LEARNING: printf("LEARNING \n");break;
+		case PRECISION: printf("PRECISION \n");break;
+		case EMPTY: printf("EMPTY \n");break;
+		default: printf("ERROR \n");
+	}
+}
+
+void quickPrintLoot(Loot loot){
+	if(loot.type == EQUIPMENT){
+		quickPrintEquipment(loot.equipment);
+	}
+	else{
+		quickPrintConsummable(loot.consummable);
+	}
+}
+
+
+void quickPrintTreasure(Treasure chest){
+
+	int i;
+
+    printf("****TREASURE CHEST****\n");
+
+    for( i = 0 ; i < 2 ; i++){
+    	printf("item %d : \n", i);
+		quickPrintLoot(chest.loot[i]);
+    }
 }
--- src/treasure.h
+++ src/treasure.h
@@ -10,37 +10,62 @@
 #define __TREASURE__
 
     /*Define*/
-    #define MAX_RARITY            5
-    #define MAX_LOOT_PER_TREASURE 5
+    #define MAX_RARITY            5 /* The maximum rarity of a loot */
+    #define MAX_LOOT_PER_TREASURE 2 /* The maximum number of loot in a treasure */
 
 
     /*Struct*/
 
     typedef enum {
-        ENERGY, /* = potion*/
-        ARMOR,
-        WEAPON,
-        WAND
+        HEAL, /* Heal 10% of max hp */
+        MAGIC, /* Heal 10% of max mp */
+        REGEN, /* Heal 20 hp and 10 mp every 3 turn for 30 turn, only one at a time */
+        LEARNING, /* Up the critical rate by 10 point of percentage for 30 turn*/
+        PRECISION, /*Up by 30% the exp gained for 30 turn*/
+        EMPTY /* None equivalent for Consummables */
+    } Consummables;
+
+    typedef enum {
+        ARMOR, /* Up the defense stat */
+        WEAPON, /* Up the attack stat */
+        WAND, /* Up the intelligence stat */
+        NONE /* None equivalent for EquipType */
+    } EquipType;
+
+    typedef struct {
+        unsigned int rarity; /* ??????????????????????????? */
+        unsigned int quality; /* Value added to the base stat */
+        EquipType type; /* Type of equipment */
+    } Equipment;
+
+    typedef enum {
+        EQUIPMENT,
+        CONSUMMABLE
+    } LootType;
+
+    typedef struct{
+        LootType type; /* Type of loot */
+        union {
+            Equipment equipment; /* Change the base stat of the player */
+            Consummables consummable; /* Change the current stat of the player */
+        };
     } Loot;
-
-
-    /*Functions*/
 
     typedef struct {
         Loot loot[MAX_LOOT_PER_TREASURE]; /*Basically, a treasure is a box full of loot*/
     } Treasure;
 
+    /*Functions*/
 
     /* Returns a randomly generated item based on the current stage and a rarityRate [1-?]
      * (= probability that a generated item may be a rare item) */
-    Loot generateLoot(unsigned int stage, unsigned int rarityRate);
+    Loot generateLoot(unsigned int quality, unsigned int rarity, LootType type);
 
     /* Returns a randomly generated loot box based on the current stage
-     * and rarity integer ([1-?]).
-     * The player's level argument will determine the average quality of the loot,
-     * and the rarity argument will determine if the treasure may contain rare items
-     * (also based on the current stage). */
-    Treasure generateTreasure(unsigned int stage, unsigned int rarity);
+     * The player's level argument will determine the average quality of the loot,*/
+    Treasure generateTreasure(unsigned int stage, unsigned int playerlevel);
 
-    /* ADD OTHER FUNCTIONS HERE */
+    /*Debug*/
+    void quickPrintTreasure(Treasure chest);
+
 #endif
