317a890558a4ce5ee54c0dc5c9f14316974dc501
MAZEYRAC <nicolas.mazeyrac@edu.univ-eiffel.fr>
2022-01-12 19:10:22 +0100

    to make tests

---
+++ src/test.h
@@ -0,0 +1,15 @@
+/* Auteurs : Nicolas Mazeyrac, Lysandre Macke
+ * Creation : 09/01/2022
+ * Modification : 09/01/2022*/
+
+/*Bibliothèque générale*/
+
+/*Bibliothèque interne*/
+#include "gameControl.h"
+#include "save.h"
+#include "turn.h"
+
+#ifndef __TEST__
+#define __TEST__
+
+#endif
